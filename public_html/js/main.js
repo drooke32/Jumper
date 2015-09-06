89632325b685c80d4fc2bec4c3942d757e4f1400
Denis Rooke <denis.rooke35@gmail.com>
2015-09-05 21:32:19 -0600

    Working on physics
    have acceleration and decel in place, starting on gravity effects -
    mostly working, some number tweaking needs to be done. Also need to make
    the space bar and arrow key presses separate from eachother so they can
    be pressed at the same time

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,18 +2,31 @@
 var canvas = document.getElementById('mainCanvas');
 canvas.width = window.innerWidth * .8;
 canvas.height = window.innerHeight * .8;
+var normalizer = 10;
+var leftBoundary = 20;
+var rightBoundary = canvas.width - 70;
+var bottomBoundary = canvas.height - 70;
+var topBoundary = 70;
 //get the canvas context
 var ctx = canvas.getContext('2d'); 
 //setup the player and it's starting co-ordinates
 var player = {
-    x:20,
-    y:canvas.height-70,
-    h:50,
-    w:50,
-    gravity:10,
-    decel:10
+    x: 20,
+    y: canvas.height-70,
+    h: 50,
+    w: 50,
+    velocity: 0,
+    acceleration: 20,
+    maxVelocity: 200,
+    friction: 5,
+    lift: 0,
+    gravity: 50
 };
 
+/**
+ * 
+ * @returns {undefined}
+ */
 var redraw = function(){
     //fill the canvas with white
     ctx.fillStyle = "#fff";
@@ -21,28 +34,109 @@ var redraw = function(){
     ctx.strokeRect(20, 20, canvas.width-40, canvas.height-40);
 
     //draw the player rectangle based on the stored player coordinates
-    ctx.strokeRect(player.x, player.y, player.h, player.w);
+    validateCoordinates();
+    ctx.strokeRect(currentXPosition(), currentYPosition(), player.h, player.w);
+    physics();
     requestAnimationFrame(redraw);
 };
 
-function moveRight(){
-    //if the right side of the player box is already at the rightmost edge of the canvas, don't move
-    if(player.x + 20 > canvas.width - 70){
-        return;
-    }
-    player.x = player.x + 20;
-}
-function moveLeft(){
-    //if the right side of the player box is already at the leftmost edge of the canvas, don't move
-    if(player.x === 20){
-        return;
-    }
-    player.x = player.x - 20;
-}
-function jump(){
-    player.y = player.y - 20;
+/**
+ * 
+ * @returns {Number|normalizer|player.xplayer.velocity}
+ */
+function currentXPosition(){
+    return player.x += (player.velocity/normalizer);
 }
 
+/**
+ * 
+ * @returns {player.yplayer.lift|Number|normalizer}
+ */
+function currentYPosition(){
+    
+    //once platforms are in, check if the y coordinate matches any of their areas
+    //for now, just check if the y coordinate is greater than or = to floor
+    if(player.y + (player.lift/normalizer) >= bottomBoundary){
+        player.lift = 0;
+        player.y = bottomBoundary;
+    }
+    return player.y += (player.lift/normalizer);
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function validateCoordinates(){
+    if(player.x < leftBoundary){
+        player.x = 20;
+    }
+    else if(player.x > rightBoundary){
+        player.x = rightBoundary;
+    }
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function moveRight(){
+    //increase the players velocity to the right
+    player.velocity = player.velocity + player.acceleration > player.maxVelocity ? player.velocity = player.maxVelocity : player.velocity += player.acceleration;
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function moveLeft(){
+    //increase the players velocity to the left
+    player.velocity = player.velocity - player.acceleration < -player.maxVelocity ? player.velocity = -player.maxVelocity : player.velocity -= player.acceleration;
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function jump(){
+    player.lift = -500;
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function physics(){
+    decelerate();
+    gravity();
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function decelerate(){
+    if (player.velocity > 0) {
+        player.velocity -= player.friction;
+    }else if(player.velocity < 0){
+        player.velocity += player.friction;
+    }else if(player.velocity < player.friction || player.velocity > -player.friction){
+        player.velocity = 0;
+    }
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function gravity(){
+    player.lift += player.gravity;
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
 function bindMovementKeys(){
     $(document).keydown(function(e) {
         switch(e.which) {
